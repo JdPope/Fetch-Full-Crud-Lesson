2d3b1e02e06e98fff27926512dfefdc7704fee87
Jessica Pope <jessica.pope.co@gmail.com>
2020-02-27 14:18:18 -0700

    2/27/20 Class Code Along

--- FrontENd/index.js
+++ FrontENd/index.js
@@ -1,53 +1,49 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  const commentsUl = document.getElementById('commentsUl')
-  const submitButton = document.getElementById('submit_Button')
-  const textBox = document.getElementById('comments_input')
+
 
   fetch('http://localhost:3000/comments')
-  .then(response => response.json())
-  .then(result => {
-    console.log(result)
-    renderComments(result)
-  })
+    .then(response => response.json())
+    .then(result => handleCommentData(result))
 
-  function renderComments(result){
-    result.map(comment =>{
-      let li = document.createElement('li')
-      li.innerText = comment.content
-      let deleteButton = document.createElement('button')
-      deleteButton.innerText = 'delete'
-      deleteButton.addEventListener('click', ()=>{
-        event.target.parentNode.remove()
-        deleteComment(comment.id)
+
+    function handleCommentData(comments){
+      console.table(comments)
+      comments.map(comment => {
+          renderId(comment)
+          renderComment(comment.content)
       })
-      li.appendChild(deleteButton)
-      commentsUl.appendChild(li)
-    })
-  }
- 
-  submitButton.addEventListener('click', ()=>{
-    event.preventDefault()
-    let newComment = textBox.value
-    postComment(newComment)
-  })
-
-  function postComment(newComment){
-    fetch('http://localhost:3000/comments',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json',
-        'Accept':'application/json'
-      },
-      body:JSON.stringify({content:newComment})
-    })
-  }
-
-  function deleteComment(chair){
-    fetch(`http://localhost:3000/comments/${chair}`, {
-      method:'DELETE'
-    })
-  }
+    }
 
 
+    function renderId(comment){
+      console.log('renderId', comment)
+    }
+
+    function renderComment(bananas){
+      const commentsContainer = document.querySelector('#commentsUl')
+      const commentContent = document.createElement('li')
+      commentContent.textContent = bananas
+      commentsContainer.appendChild(commentContent)
+    }
+
+
+        const commentsForm = document.querySelector('#comments-form')
+
+        commentsForm.addEventListener('submit', ()=>{
+          event.preventDefault()
+          getUserComment(commentsForm)
+        })
+
+    function getUserComment(commentsForm){
+      const newFormData = new FormData(commentsForm)
+      const formComment = newFormData.get('comment')
+      renderComment(formComment)
+      sendUserComment(formComment)
+    }
+
+
+    function sendUserComment(){
+        
+    }
 
 })
